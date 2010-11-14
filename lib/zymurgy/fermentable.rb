132738cf8d4a3d6dbd03bfad9ad14d81d414ba8d
Mike Breeze <mike.breeze@rea-group.com>
2010-11-14 15:31:42 +1100

    (beer 19) Added gravity point calulations to Zymurgy::Fermentable

--- lib/zymurgy/fermentable.rb
+++ lib/zymurgy/fermentable.rb
@@ -17,5 +17,17 @@ module Zymurgy
       @points_per_kg_per_litre = params['points_per_kg_per_litre'] || DEFAULT['points_per_kg_per_litre']
       @weight_in_kg = params['weight_in_kg'] || DEFAULT['weight_in_kg']
     end
+
+    def volume_gravity_points volume_litres
+      (@points_per_kg_per_litre / volume_litres) * @weight_in_kg * @brew.brewery.efficiency_percentage / 100      
+    end
+
+    def post_boil_volume_gravity_points
+      volume_gravity_points @brew.post_boil_volume_litres
+    end
+
+    def pre_boil_volume_gravity_points
+      volume_gravity_points @brew.mash_out_volume_litres
+    end
   end
 end
--- spec/zymurgy_fermentable_spec.rb
+++ spec/zymurgy_fermentable_spec.rb
@@ -12,6 +12,10 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 ##      Most grains need to be mashed (soaked in hot water) before fermenting to
 ##      release their sugars. Fruit, honey, sugars obviously don't need to be.
 ##
+##      Efficiency or Mash Efficiency represents the proportion of the total sugars
+##      in the grain that are made available by the mashing process. Efficiency is
+##      unique to each brewhouse.
+##
 ##      The sugars contained in fermentables are measured in terms of Specific Gravity.
 ##      The starting or original gravity of a beer represents the amount of sugar
 ##      present at the start of the fermentation. Each fermentable contributes
@@ -22,23 +26,28 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 ##      For the brew itself, the sugars contributed are measured in terms of points
 ##      per kg (or pound) per litre (or gallon) of fermenter volume.
 ##
-##      E.g 5.24kg of pilsner malt (which yields 300 points per kg per litre) in a
-##          27l fermenter, gives up 35.52 gravity points to give an original gravity
+##      E.g 5.24kg of pilsner malt (which yields 300 points per kg per litre at 100%
+##          efficiency) in a 27l fermenter, gives up 35.52 gravity points after mashing
+##          with an efficiency of 61%. This provides an original gravity
 ##          of 1.03552 for the brew.
 ##
 
 # TODO: Is mashable? Can that be a mixin?
 describe "Zymurgy" do
   before do
+    @brewery = mock('A Brewery')
+    @brewery.stub!('efficiency_percentage').and_return(61)    
     @brew = mock('A Brew Recipe')
     @brew.stub!('class').and_return('Zymurgy::Brew')
+    @brew.stub!('brewery').and_return(@brewery)
     @brew.stub!('original_gravity').and_return(1045)
-    @brew.stub!('fermentation_volume_litres').and_return(25)
-    @brew.stub!('post_boil_volume_litres').and_return(27)
+    @brew.stub!('fermentation_volume_litres').and_return(25.0)
+    @brew.stub!('post_boil_volume_litres').and_return(27.0)
+    @brew.stub!('mash_out_volume_litres').and_return(38)
 
     @params = {
         'mash' => true,
-        'points_per_kg_per_litre' => 300,
+        'points_per_kg_per_litre' => 300.0,
         'weight_in_kg' => 5.24,
     }
   end
@@ -70,7 +79,24 @@ describe "Zymurgy" do
         fermentable.points_per_kg_per_litre.should == 300
         fermentable.weight_in_kg.should == 1.7
       end
-      
+    end
+
+    # TODO: Only include efficiency when mash == true.
+    describe "Performing fermentable calculations" do
+      it "should calculate the gravity points contributed towards the post boil volume" do
+        fermentable = Zymurgy::Fermentable.new(@brew, @params)
+        fermentable.post_boil_volume_gravity_points.round_dp(2).should == 35.52
+      end
+
+      it "should calculate the gravity points contributed towards the mash out/pre boil volume" do
+        fermentable = Zymurgy::Fermentable.new(@brew, @params)
+        fermentable.pre_boil_volume_gravity_points.round_dp(2).should == 25.23
+      end
+
+      it "should calculate the gravity points contributed towards a given volume" do
+        fermentable = Zymurgy::Fermentable.new(@brew, @params)
+        fermentable.volume_gravity_points(38).round_dp(2).should == 25.23
+      end
     end
   end
 end
