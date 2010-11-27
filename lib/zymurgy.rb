87df7ebcf5666e8dd0ca27b9804a6b6188de2ad7
Mike Breeze <mike.breeze@rea-group.com>
2010-11-27 22:06:13 +1100

    Started conversion to acts_as plugin style modules.

--- lib/zymurgy.rb
+++ lib/zymurgy.rb
@@ -1,12 +1,18 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
+require 'rubygems'
+require 'active_record'
+require "zymurgy/acts_as_brewery"
+require "zymurgy/acts_as_brew_recipe"
+
 module Zymurgy
 
-  autoload :Brewery,     "zymurgy/brewery"
-  autoload :Brew,        "zymurgy/brew"
-  autoload :Fermentable, "zymurgy/fermentable"
-  autoload :HopAddition, "zymurgy/hop_addition"
-  autoload :Utils,       "zymurgy/utils"
+  autoload :Brewery,       "zymurgy/brewery"
+  autoload :Brew,          "zymurgy/brew"
+  autoload :Fermentable,   "zymurgy/fermentable"
+  autoload :HopAddition,   "zymurgy/hop_addition"
+  autoload :Utils,         "zymurgy/utils"
+  autoload :ActsAsBrewery, "zymurgy/acts_as_brewery"
 
   ZymurgyError = Class.new(StandardError)
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'rubygems'
 require 'zymurgy'
 require 'spec'
 require 'spec/autorun'
@@ -11,4 +12,13 @@ require 'patches/float'
 Spec::Runner.configure do |config|
   config.include Zymurgy::Utils
 
+end
+#require 'active_support'
+require 'active_record'
+
+
+def load_schema
+  config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
+  ActiveRecord::Base.establish_connection(config['mysql'])
+  load(File.dirname(__FILE__) + "/schema.rb")
 end
